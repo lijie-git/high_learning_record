5a12220f76265eb7f7e8a06144acec95c242fc86
lj <2925321452@qq.com>
2020-07-16 09:29:59 +0800

    代码修改

--- src/main/java/lijie/test/kafka/KafkaAsynProducer.java
+++ src/main/java/lijie/test/kafka/KafkaAsynProducer.java
@@ -24,7 +24,7 @@ public class KafkaAsynProducer implements Callable {
     public Object call() throws Exception {
         KafkaRequest kafkaRequest = new KafkaRequest();
         kafkaRequest.setOrgId(1);
-        kafkaRequest.setAccessName("芒果动力");
+        kafkaRequest.setAccessName("水果");
         kafkaRequest.setAccessType("api接入");
         kafkaRequest.setSessionId("1");
         kafkaRequest.setUniqueId("1234");
@@ -32,39 +32,27 @@ public class KafkaAsynProducer implements Callable {
 
         KafkaRequest kafkaRequest1 = new KafkaRequest();
         kafkaRequest1.setOrgId(1);
-        kafkaRequest1.setAccessName("芒果动力");
+        kafkaRequest1.setAccessName("水果");
         kafkaRequest1.setAccessType("api接入");
-        kafkaRequest1.setSessionId("22");
-        kafkaRequest1.setUniqueId("1234");
+        kafkaRequest1.setSessionId("2222");
+        kafkaRequest1.setUniqueId("1234567812");
         KafkaProducer<String, String> producer = ProducerKafka.getProducer();
         try {
-            for (int i = 0; i < 1; i++) {
-                if (i == 0) {
-                    producer.send(new ProducerRecord("TestTopic", "test", JSONObject.toJSONString(kafkaRequest1)), new Callback() {
-                        public void onCompletion(RecordMetadata metadata, Exception exception) {
-                            if (null != exception) {
-                                exception.printStackTrace();
-                            }
-                            if (null != metadata) {
-                                System.out.println("offset: " + metadata.offset() + " "
-                                        + "partition: " + metadata.partition());
-                            }
+            for (int i = 15; i < 20; i++) {
+                kafkaRequest1.setSessionId(String.valueOf(i));
+                producer.send(new ProducerRecord("TestTopic", "test", JSONObject.toJSONString(kafkaRequest1)), new Callback() {
+                    public void onCompletion(RecordMetadata metadata, Exception exception) {
+                        if (null != exception) {
+                            exception.printStackTrace();
                         }
-                    });
-                } else {
-                    producer.send(new ProducerRecord("TestTopic", "test", JSONObject.toJSONString(kafkaRequest)), new Callback() {
-                        public void onCompletion(RecordMetadata metadata, Exception exception) {
-                            if (null != exception) {
-                                exception.printStackTrace();
-                            }
-                            if (null != metadata) {
-                                System.out.println("offset: " + metadata.offset() + " "
-                                        + "partition: " + metadata.partition());
-                            }
+                        if (null != metadata) {
+                            System.out.println("offset: " + metadata.offset() + " "
+                                    + "partition: " + metadata.partition());
                         }
-                    });
-                }
+                    }
+                });
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
